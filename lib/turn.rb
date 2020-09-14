ab34310915eea73bc9a05084a9fa52041ac459e0
Jonathan Bernstein <jjbernstein@gmail.com>
2020-09-14 15:24:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,30 +1,3 @@
-# code your #valid_move? method here
-def valid_move?(board, position)
-  position = position.to_i - 1
-  if 
-    position <0 || position >8
-    false
-  elsif position_taken?(board, position) == false
-    true
-  elsif
-    position_taken?(board, position) == true
-    false
-  else
-    false
-  end
-  
-end  
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, position)
-if board[position] == " " || board[position] == "" || board[position] == nil
-   false
-  elsif board[position] == "X" || board[position] == "O" 
-    true
-else
-   true
-end  
-end
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -34,13 +7,39 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def turn(board)
-  user_input  = 0
-  puts "Please enter 1-9:"
+   def valid_move?(board, position)
+    position = position.to_i
+    position = position-1
+    if position <0 || position >8
+      false
+    elsif board[position] == "X" || board[position] == "O"
+      false
+     
+    elsif board[position] == " " || board[position] == ""
+      true
+    else
+      true
+    end
 
-  while user_input < 1 || user_input > 9
-    user_input = gets.strip.to_i
   end
-    valid_move(board,user_input - 1)
-    
+
+  def move(board, position, token = "X")
+        position = position.to_i
+    position = position-1
+    board[position] = token
+    return board
+
+  end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets
+  if valid_move?(board, position)
+    token = "X"
+    move(board, position, token)
+    display_board(board)
+    return board
+  else
+    turn(board)
+  end
 end
